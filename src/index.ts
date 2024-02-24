53f0ecd2402113d440652e1a819e25db0eadf954
Luligu <132135057+Luligu@users.noreply.github.com>
2024-02-24 12:07:27 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -41,57 +41,12 @@ export default function initializePlugin(matterbridge: Matterbridge, log: AnsiLo
   const storageManager = new StorageManager(storageJson);
   const matterServer = new MatterServer(storageManager);
 
-  log.info('My Plugin is loading...');
+  log.info('Matterbridge dynamic platform plugin example is loading...');
 
-  //const platform = new ExampleMatterbridgePlatform(matterbridge, log);
   const platform = new ExampleMatterbridgeDynamicPlatform(matterbridge, log);
 
-  log.info('My Plugin initialized successfully!');
+  log.info('Matterbridge dynamic platform plugin example initialized successfully!');
   return platform
-}
-
-class ExampleMatterbridgePlatform extends MatterbridgePlatform {
-
-  constructor(matterbridge: Matterbridge, log: AnsiLogger) {
-    super(matterbridge, log);
-    log.debug(`ExampleMatterbridgePlatform loaded (matterbridge is running on node v${matterbridge.nodeVersion})`);
-  }
-
-  override onStartPlatform(): void {
-    this.log.info(`onStartPlatform called (matterbridge is running on node v${this.matterbridge.nodeVersion})`);
-
-    const matterDevice1 = new MatterbridgeDevice(DeviceTypes.WINDOW_COVERING);
-    matterDevice1.createDefaultIdentifyClusterServer();
-    matterDevice1.createDefaultBasicInformationClusterServer('Device1', 'Device1 0x01020304', 0xfff1, 'Luligu', 0x0001, 'Device1');
-    matterDevice1.createDefaultOnOffClusterServer();
-    matterDevice1.createDefaultWindowCoveringClusterServer(0);
-    this.registerDevice(matterDevice1)
-
-    matterDevice1.addCommandHandler('identify', async ({ request: { identifyTime } }) => {
-      this.log.warn(`Command identify called identifyTime:${identifyTime}`);
-    });
-
-    matterDevice1.addCommandHandler('goToLiftPercentage', async ({ request: { liftPercent100thsValue } }) => {
-      this.log.warn(`Command goToLiftPercentage called liftPercent100thsValue:${liftPercent100thsValue}`);
-    });
-
-    /*
-    const matterDevice2: Device = new Device(DeviceTypes.CONTACT_SENSOR);
-    matterDevice2.addClusterServer(createDefaultIdentifyClusterServer());
-    matterDevice2.addClusterServer(createDefaultBasicInformationClusterServer('Device2', 'Device2 0x01450304', 0xfff1, 'Luligu', 0x0002, 'Device2 contact'));
-    matterDevice2.addClusterServer(createDefaultBooleanStateClusterServer(true));
-    //matterbridge.addDevice(matterDevice2);
-
-    const composed1 = new ComposedDevice(DeviceTypes.WINDOW_COVERING, [matterDevice1]);
-    matterbridge.addDevice(composed1 as unknown as Device);
-    const composed2 = new ComposedDevice(DeviceTypes.CONTACT_SENSOR, [ matterDevice2]);
-    matterbridge.addDevice(composed2 as unknown as Device);
-    */
-  }
-
-  override onShutdown(): void {
-    this.log.info(`onShutdown called (matterbridge is running on node v${this.matterbridge.nodeVersion})`);
-  }
 }
 
 class ExampleMatterbridgeDynamicPlatform extends MatterbridgeDynamicPlatform {
