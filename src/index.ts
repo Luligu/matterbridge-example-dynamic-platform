e1d8b491b3d37d680c4978b6a2d5b1070a1ceaa9
Luligu <132135057+Luligu@users.noreply.github.com>
2024-03-09 21:48:37 +0100

    1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import { MatterServer } from '@project-chip/matter-node.js';
-import { Format, Level, Logger } from '@project-chip/matter-node.js/log';
-import { StorageBackendJsonFile, StorageManager } from '@project-chip/matter-node.js/storage';
-
-import { Matterbridge } from '../../matterbridge/dist/index.js';
+import { Matterbridge } from 'matterbridge';
 import { AnsiLogger } from 'node-ansi-logger';
 import { ExampleMatterbridgeDynamicPlatform } from './platform.js';
 
@@ -13,19 +9,5 @@ import { ExampleMatterbridgeDynamicPlatform } from './platform.js';
  * @param matterbridge - An instance of MatterBridge
  */
 export default function initializePlugin(matterbridge: Matterbridge, log: AnsiLogger) {
-  // set matter.js logger level and format
-  Logger.defaultLogLevel = Level.DEBUG;
-  Logger.format = Format.ANSI;
-
-  // Do nothing just load @project-chip/matter-node.js for the Time Crypto Net Node variant
-  const storageJson = new StorageBackendJsonFile('matterbridge-example');
-  const storageManager = new StorageManager(storageJson);
-  new MatterServer(storageManager);
-
-  log.info('Matterbridge dynamic platform plugin example is loading...');
-
-  const platform = new ExampleMatterbridgeDynamicPlatform(matterbridge, log);
-
-  log.info('Matterbridge dynamic platform plugin example initialized successfully!');
-  return platform;
+  return new ExampleMatterbridgeDynamicPlatform(matterbridge, log);
 }
--- src/platform.ts
+++ src/platform.ts
@@ -1,6 +1,6 @@
-import { DeviceTypes } from '@project-chip/matter-node.js/device';
+import { DeviceTypes, OnOffCluster, WindowCovering, WindowCoveringCluster } from 'matterbridge';
 
-import { Matterbridge, MatterbridgeDevice, MatterbridgeDynamicPlatform } from '../../matterbridge/dist/index.js';
+import { Matterbridge, MatterbridgeDevice, MatterbridgeDynamicPlatform } from 'matterbridge';
 import { AnsiLogger } from 'node-ansi-logger';
 
 export class ExampleMatterbridgeDynamicPlatform extends MatterbridgeDynamicPlatform {
@@ -8,46 +8,78 @@ export class ExampleMatterbridgeDynamicPlatform extends MatterbridgeDynamicPlatf
     super(matterbridge, log);
   }
 
-  override onStartDynamicPlatform(): void {
-    this.log.info('onStartDynamicPlatform called');
+  override async onStart(reason?: string) {
+    this.log.info('onStart called with reason:', reason ?? 'none');
 
     const cover = new MatterbridgeDevice(DeviceTypes.WINDOW_COVERING);
     cover.createDefaultIdentifyClusterServer();
     cover.createDefaultGroupsClusterServer();
     cover.createDefaultScenesClusterServer();
-    cover.createDefaultBridgedDeviceBasicInformationClusterServer('BridgedDevice1', 'BridgedDevice1 0x01020564', 0xfff1, 'Luligu', 'BridgedDevice1');
+    cover.createDefaultBridgedDeviceBasicInformationClusterServer('Bridged device 1', '0x01020564', 0xfff1, 'Luligu', 'Dynamic device 1');
     cover.createDefaultPowerSourceRechargableBatteryClusterServer(86);
     cover.createDefaultWindowCoveringClusterServer();
-    this.registerDevice(cover);
+    await this.registerDevice(cover);
+
+    setInterval(
+      () => {
+        const coverCluster = cover.getClusterServer(WindowCoveringCluster.with(WindowCovering.Feature.Lift, WindowCovering.Feature.PositionAwareLift));
+        if (coverCluster && coverCluster.getCurrentPositionLiftPercent100thsAttribute) {
+          let position = coverCluster.getCurrentPositionLiftPercent100thsAttribute();
+          if (position === null) return;
+          position = position >= 9000 ? 0 : position + 1000;
+          coverCluster.setTargetPositionLiftPercent100thsAttribute(position);
+          coverCluster.setCurrentPositionLiftPercent100thsAttribute(position);
+          coverCluster.setOperationalStatusAttribute({
+            global: WindowCovering.MovementStatus.Stopped,
+            lift: WindowCovering.MovementStatus.Stopped,
+            tilt: WindowCovering.MovementStatus.Stopped,
+          });
+          this.log.info(`Set PositionLiftPercent100ths to ${position}`);
+        }
+      },
+      60 * 1000 + 500,
+    );
 
     cover.addCommandHandler('identify', async ({ request: { identifyTime } }) => {
-      this.log.warn(`Command identify called identifyTime:${identifyTime}`);
+      this.log.info(`Command identify called identifyTime:${identifyTime}`);
     });
     cover.addCommandHandler('goToLiftPercentage', async ({ request: { liftPercent100thsValue } }) => {
-      this.log.warn(`Command goToLiftPercentage called liftPercent100thsValue:${liftPercent100thsValue}`);
+      this.log.info(`Command goToLiftPercentage called liftPercent100thsValue:${liftPercent100thsValue}`);
     });
 
     const light = new MatterbridgeDevice(DeviceTypes.ON_OFF_LIGHT);
     light.createDefaultIdentifyClusterServer();
     light.createDefaultGroupsClusterServer();
     light.createDefaultScenesClusterServer();
-    light.createDefaultBridgedDeviceBasicInformationClusterServer('BridgedDevice2', 'BridgedDevice2 0x23023304', 0xfff1, 'Luligu', 'BridgedDevice2');
+    light.createDefaultBridgedDeviceBasicInformationClusterServer('Bridged device 2', '0x23480564', 0xfff1, 'Luligu', 'Dynamic device 2');
     light.createDefaultPowerSourceReplaceableBatteryClusterServer(70);
     light.createDefaultOnOffClusterServer();
     this.registerDevice(light);
 
+    setInterval(
+      () => {
+        const lightCluster = light.getClusterServer(OnOffCluster);
+        if (lightCluster) {
+          const status = lightCluster.getOnOffAttribute();
+          lightCluster.setOnOffAttribute(!status);
+          this.log.info(`Set onOff to ${!status}`);
+        }
+      },
+      60 * 1000 + 200,
+    );
+
     light.addCommandHandler('identify', async ({ request: { identifyTime } }) => {
-      this.log.warn(`Command identify called identifyTime:${identifyTime}`);
+      this.log.info(`Command identify called identifyTime:${identifyTime}`);
     });
     light.addCommandHandler('on', async () => {
-      this.log.warn('Command on called');
+      this.log.info('Command on called');
     });
     light.addCommandHandler('off', async () => {
-      this.log.warn('Command off called');
+      this.log.info('Command off called');
     });
   }
 
-  override onShutdown(): void {
-    this.log.info('onShutdown called');
+  override async onShutdown(reason?: string) {
+    this.log.info('onShutdown called with reason:', reason ?? 'none');
   }
 }
