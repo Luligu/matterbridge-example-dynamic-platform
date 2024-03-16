d68af96c6127f17566324503b87b4b61fd217766
Luligu <132135057+Luligu@users.noreply.github.com>
2024-03-16 09:06:25 +0100

    Update platform.ts

--- src/platform.ts
+++ src/platform.ts
@@ -6,6 +6,8 @@ import { AnsiLogger } from 'node-ansi-logger';
 export class ExampleMatterbridgeDynamicPlatform extends MatterbridgeDynamicPlatform {
   cover: MatterbridgeDevice | undefined;
   light: MatterbridgeDevice | undefined;
+  coverInterval: NodeJS.Timeout | undefined;
+  lightInterval: NodeJS.Timeout | undefined;
 
   constructor(matterbridge: Matterbridge, log: AnsiLogger) {
     super(matterbridge, log);
@@ -66,7 +68,7 @@ export class ExampleMatterbridgeDynamicPlatform extends MatterbridgeDynamicPlatf
       });
     }
 
-    setInterval(
+    this.coverInterval = setInterval(
       () => {
         if (!this.cover) return;
         const coverCluster = this.cover.getClusterServer(WindowCoveringCluster.with(WindowCovering.Feature.Lift, WindowCovering.Feature.PositionAwareLift));
@@ -87,7 +89,7 @@ export class ExampleMatterbridgeDynamicPlatform extends MatterbridgeDynamicPlatf
       60 * 1000 + 500,
     );
 
-    setInterval(
+    this.lightInterval = setInterval(
       () => {
         if (!this.light) return;
         const lightCluster = this.light.getClusterServer(OnOffCluster);
@@ -103,5 +105,7 @@ export class ExampleMatterbridgeDynamicPlatform extends MatterbridgeDynamicPlatf
 
   override async onShutdown(reason?: string) {
     this.log.info('onShutdown called with reason:', reason ?? 'none');
+    clearInterval(this.coverInterval);
+    clearInterval(this.lightInterval);
   }
 }
