08df875e14b74ce7933a3d72dca48a859fbf6ba4
Luligu <132135057+Luligu@users.noreply.github.com>
2024-10-28 10:22:07 +0100

    Release 1.0.23

--- src/platform.test.ts
+++ src/platform.test.ts
@@ -22,10 +22,10 @@ describe('TestPlatform', () => {
     } as unknown as Matterbridge;
     mockLog = { fatal: jest.fn(), error: jest.fn(), warn: jest.fn(), notice: jest.fn(), info: jest.fn(), debug: jest.fn() } as unknown as AnsiLogger;
     mockConfig = {
-      'name': 'matterbridge-test',
+      'name': 'matterbridge-example-dynamic-platform',
       'type': 'DynamicPlatform',
       'unregisterOnShutdown': false,
-      'delayStart': false,
+      'debug': false,
     } as PlatformConfig;
 
     testPlatform = new ExampleMatterbridgeDynamicPlatform(mockMatterbridge, mockLog, mockConfig);
