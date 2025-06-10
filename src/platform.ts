770acdb0a2f90f51c9450ff566db184b59f44196
Ludovic BOUE <lboue@users.noreply.github.com>
2025-06-10 20:46:05 +0200

    Change to State.PluggedInDemand

--- src/platform.ts
+++ src/platform.ts
@@ -1507,7 +1507,7 @@ export class ExampleMatterbridgeDynamicPlatform extends MatterbridgeDynamicPlatf
       { label: 'Scheduled', mode: 2, modeTags: [{ value: EnergyEvseMode.ModeTag.TimeOfUse }] },
       { label: 'Solar Charging', mode: 3, modeTags: [{ value: EnergyEvseMode.ModeTag.SolarCharging }] },
       { label: 'Solar Charging Scheduled', mode: 4, modeTags: [{ value: EnergyEvseMode.ModeTag.SolarCharging }, { value: EnergyEvseMode.ModeTag.TimeOfUse }] },
-    ], 1, 1, 0, 1680, 3680);
+    ], 2, 1, 0, 1680, 3680);
     this.setSelectDevice(this.evse.serialNumber ?? '', this.evse.deviceName ?? '', undefined, 'hub');
     if (this.validateDevice(this.evse.deviceName ?? '')) {
       await this.registerDevice(this.evse);
