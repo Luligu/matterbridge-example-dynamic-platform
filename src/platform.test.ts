2f658201afd968e93dea2c68c8595af9cc0be238
Ludovic BOUE <lboue@users.noreply.github.com>
2025-07-29 21:04:49 +0200

    Update logger call count in platform tests

--- src/platform.test.ts
+++ src/platform.test.ts
@@ -344,7 +344,7 @@ describe('TestPlatform', () => {
 
     expect(mockLog.info).toHaveBeenCalledTimes(5);
     expect(mockLog.error).toHaveBeenCalledTimes(0);
-    expect(loggerLogSpy).toHaveBeenCalledTimes(2546);
+    expect(loggerLogSpy).toHaveBeenCalledTimes(2669);
     expect(loggerLogSpy).toHaveBeenCalledWith(LogLevel.INFO, expect.stringContaining('Switch.SinglePress'));
     expect(loggerLogSpy).toHaveBeenCalledWith(LogLevel.INFO, expect.stringContaining('Switch.DoublePress'));
     expect(loggerLogSpy).toHaveBeenCalledWith(LogLevel.INFO, expect.stringContaining('Switch.LongPress'));
